6e81d96638945134b512156ca48a07438402c748
Shiraizawa <dorazoltan010416@gmail.com>
2023-05-16 16:17:34 +0200

    Moved TargetFPS to globals

--- WpfApp2/Handlers/TickRate/TickHandler.cs
+++ WpfApp2/Handlers/TickRate/TickHandler.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp2.Config;
 
 namespace WpfApp2.Handlers.TickRate
 {
     internal class TickHandler
     {
-        private const int TargetFPS = 30; // pl. 30 FPS
+       
         public readonly System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
         public TickHandler()
         {
-            timer.Interval = TimeSpan.FromMilliseconds(1000.0 / TargetFPS);
+            timer.Interval = TimeSpan.FromMilliseconds(1000.0 / Globals.TargetFPS);
             timer.Start();
 
         }
