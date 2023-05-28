55a03a2949f4262d82623799e3fef0baeb82682d
rolandvaradi4 <87499689+rolandvaradi4@users.noreply.github.com>
2023-05-28 19:41:13 +0200

    i fixed skybox now is good

--- WpfApp2/Models/Skybox.cs
+++ WpfApp2/Models/Skybox.cs
@@ -13,19 +13,19 @@ namespace WpfApp2.Models
         public Model3D CreateSkybox()
         {
             // Create the skybox mesh back
-            MeshGeometry3D meshGeometry3DBack = new MeshGeometry3D();
-            meshGeometry3DBack.Positions = new Point3DCollection(new Point3D[]
+            MeshGeometry3D meshGeometry3DBottom = new MeshGeometry3D();
+            meshGeometry3DBottom.Positions = new Point3DCollection(new Point3D[]
            {
                 new Point3D(500, 500, -500),
                 new Point3D(-500, 500, -500),
                 new Point3D(500, -500, -500),
                 new Point3D(-500, -500, -500),
            });
-            meshGeometry3DBack.TriangleIndices = new Int32Collection(new int[]
+            meshGeometry3DBottom.TriangleIndices = new Int32Collection(new int[]
             {
                0 , 1,  2,  2,  1,  3,
             });
-            meshGeometry3DBack.TextureCoordinates = new PointCollection(new Point[]
+            meshGeometry3DBottom.TextureCoordinates = new PointCollection(new Point[]
              {
 
                new Point(1, 1),
@@ -34,23 +34,23 @@ namespace WpfApp2.Models
     new Point(0, 0),
              });
 
-            BitmapImage back = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxBottom.png", UriKind.RelativeOrAbsolute));
-            GeometryModel3D GeometryBack = new GeometryModel3D(meshGeometry3DBack, new DiffuseMaterial(new ImageBrush(back)));
+            BitmapImage bottom = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxBottom.png", UriKind.RelativeOrAbsolute));
+            GeometryModel3D GeometryBottom = new GeometryModel3D(meshGeometry3DBottom, new DiffuseMaterial(new ImageBrush(bottom)));
 
 
-            MeshGeometry3D meshGeometry3DFront = new MeshGeometry3D();
-            meshGeometry3DFront.Positions = new Point3DCollection(new Point3D[]
+            MeshGeometry3D meshGeometry3DTop = new MeshGeometry3D();
+            meshGeometry3DTop.Positions = new Point3DCollection(new Point3D[]
            {
                 new Point3D(-500, 500, 500),
                 new Point3D(500, 500, 500),
                 new Point3D(-500, -500, 500),
                 new Point3D(500, -500, 500),
            });
-            meshGeometry3DFront.TriangleIndices = new Int32Collection(new int[]
+            meshGeometry3DTop.TriangleIndices = new Int32Collection(new int[]
             {
                0 , 1,  2,  2,  1,  3,
             });
-            meshGeometry3DFront.TextureCoordinates = new PointCollection(new Point[]
+            meshGeometry3DTop.TextureCoordinates = new PointCollection(new Point[]
              {
 
                 new Point(0, 0),
@@ -59,8 +59,8 @@ namespace WpfApp2.Models
                 new Point(1, 1),
              });
 
-            BitmapImage Front = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxTop.png", UriKind.RelativeOrAbsolute));
-            GeometryModel3D GeometryFront = new GeometryModel3D(meshGeometry3DFront, new DiffuseMaterial(new ImageBrush(Front)));
+            BitmapImage Top = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxTop.png", UriKind.RelativeOrAbsolute));
+            GeometryModel3D GeometryTop = new GeometryModel3D(meshGeometry3DTop, new DiffuseMaterial(new ImageBrush(Top)));
 
 
             MeshGeometry3D meshGeometry3DLeft = new MeshGeometry3D();
@@ -102,63 +102,63 @@ namespace WpfApp2.Models
             meshGeometry3DRight.TextureCoordinates = new PointCollection(new Point[]
              {
 
-             new Point(0, 0),
-    new Point(0, 1),
-    new Point(1, 0),
+            new Point(1, 0),
     new Point(1, 1),
+    new Point(0, 0),
+    new Point(0, 1),
              });
 
-            BitmapImage Right = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxFront.png", UriKind.RelativeOrAbsolute));
+            BitmapImage Right = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxRight.png", UriKind.RelativeOrAbsolute));
             GeometryModel3D GeometryRight = new GeometryModel3D(meshGeometry3DRight, new DiffuseMaterial(new ImageBrush(Right)));
 
 
-            MeshGeometry3D meshGeometry3DTop = new MeshGeometry3D();
-            meshGeometry3DTop.Positions = new Point3DCollection(new Point3D[]
+            MeshGeometry3D meshGeometry3DBack = new MeshGeometry3D();
+            meshGeometry3DBack.Positions = new Point3DCollection(new Point3D[]
            {
                 new Point3D(-500, 500, -500),
                 new Point3D(500, 500, -500),
                 new Point3D(-500, 500, 500),
                 new Point3D(500, 500, 500),
            });
-            meshGeometry3DTop.TriangleIndices = new Int32Collection(new int[]
+            meshGeometry3DBack.TriangleIndices = new Int32Collection(new int[]
             {
                0 , 1,  2,  2,  1,  3,
             });
-            meshGeometry3DTop.TextureCoordinates = new PointCollection(new Point[]
+            meshGeometry3DBack.TextureCoordinates = new PointCollection(new Point[]
              {
 
-               new Point(1, 1),
-    new Point(0, 1),
-    new Point(1, 0),
-    new Point(0, 0),
+                new Point(1, 1),
+                new Point(0, 1),
+                new Point(1, 0),
+                new Point(0, 0),
              });
 
-            BitmapImage Top = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxRight.png", UriKind.RelativeOrAbsolute));
-            GeometryModel3D GeometryTop = new GeometryModel3D(meshGeometry3DTop, new DiffuseMaterial(new ImageBrush(Top)));
+            BitmapImage Back = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxBack.png", UriKind.RelativeOrAbsolute));
+            GeometryModel3D GeometryBack = new GeometryModel3D(meshGeometry3DBack, new DiffuseMaterial(new ImageBrush(Back)));
 
 
-            MeshGeometry3D meshGeometry3DBottom = new MeshGeometry3D();
-            meshGeometry3DBottom.Positions = new Point3DCollection(new Point3D[]
+            MeshGeometry3D meshGeometry3DFront = new MeshGeometry3D();
+            meshGeometry3DFront.Positions = new Point3DCollection(new Point3D[]
            {
                 new Point3D(500, -500, -500),
                 new Point3D(-500, -500, -500),
                 new Point3D(500, -500, 500),
                 new Point3D(-500, -500, 500),
            });
-            meshGeometry3DBottom.TriangleIndices = new Int32Collection(new int[]
+            meshGeometry3DFront.TriangleIndices = new Int32Collection(new int[]
             {
                0 , 1,  2,  2,  1,  3,
             });
-            meshGeometry3DBottom.TextureCoordinates = new PointCollection(new Point[]
+            meshGeometry3DFront.TextureCoordinates = new PointCollection(new Point[]
              {
-new Point(1, 1),
-    new Point(0, 1),
-    new Point(1, 0),
-    new Point(0, 0),
+                new Point(1, 1),
+                new Point(0, 1),
+                new Point(1, 0),
+                new Point(0, 0),
              });
 
-            BitmapImage Bottom = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxBack.png", UriKind.RelativeOrAbsolute));
-            GeometryModel3D GeometryBottom = new GeometryModel3D(meshGeometry3DBottom, new DiffuseMaterial(new ImageBrush(Bottom)));
+            BitmapImage Front = new BitmapImage(new Uri("pack://application:,,,/Models/skyboxFront.png", UriKind.RelativeOrAbsolute));
+            GeometryModel3D GeometryFront = new GeometryModel3D(meshGeometry3DFront, new DiffuseMaterial(new ImageBrush(Front)));
 
             Model3DGroup model3DGroup = new Model3DGroup();
             model3DGroup.Children.Add(GeometryBack);
